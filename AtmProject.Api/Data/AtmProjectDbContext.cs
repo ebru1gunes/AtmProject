119722e91376b96aa925f0f1fd6de65a78979b54
Ebru <ebru1gunes@gmail.com>
2023-08-18 17:42:58 +0300

    Add project files.

---
+++ AtmProject.Api/Data/AtmProjectDbContext.cs
@@ -0,0 +1,21 @@
+﻿using AtmProject.Api.Data.Configurations;
+using AtmProject.Api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AtmProject.Api.Data
+{
+    public class AtmProjectDbContext:DbContext
+    {
+        public AtmProjectDbContext(DbContextOptions options) :base(options)
+        {
+        }
+
+        public DbSet<City>  City { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new CityConfiguration());
+        }
+    }
+}
---
+++ AtmProject.Api/Data/Configurations/CityConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AtmProject.Api.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AtmProject.Api.Data.Configurations
+{
+    public class CityConfiguration : IEntityTypeConfiguration<City>
+    {
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).HasMaxLength(200).IsRequired();
+                
+
+            builder.ToTable("City");
+         }
+    }
+}
